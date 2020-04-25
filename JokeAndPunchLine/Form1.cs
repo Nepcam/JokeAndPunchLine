c0b5733103263daf147d71144467e9c4372bb201
Nepcam <cameron.nepe@gmail.com>
2020-04-25 17:41:01 +1200

    Add project files.

---
+++ JokeAndPunchLine/Form1.cs
@@ -0,0 +1,28 @@
+﻿/*
+ Think of your favorite joke and identify its setup and punch line.
+ Then, create an application that has a Label and two buttons on a form.
+ One of the buttons should read "Setup" and the other button should read "Punch Line".
+ When the SetUp button is clicked, display the joke's setup in the label control.
+ When the Punch line button is clicked, display the joke's punch line in the label control
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JokeAndPunchLine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
